c70b17b84d85c5ace766f8a07b76db42e97e5165
Ben Rosebery <benrosebery@gmail.com>
2023-05-11 21:10:35 -0400

    Update add-Ben.sh

--- .init/add-Ben.sh
+++ .init/add-Ben.sh
@@ -1,6 +1,6 @@
 #!usr/bin/env bash
 
-sudo useradd -g wheel Bewnv && cd /home/
-sudo git clone https://github.com/Bewn/Bewn
+sudo useradd -g wheel Bewn
+cd /home && sudo git clone https://github.com/Bewn/Bewn
 sudo chown Bewn Bewn
 
